c460daa8b247795ec3ca41c98d6fbd82bfd8be0f
Logan C McCrorie <90468105+gamerGodElite@users.noreply.github.com>
2023-11-15 13:41:50 -0700

    Dice Roll
    Got the random dice generator to work

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
+    [SerializeField] private int die1;
+    [SerializeField] private int die2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,14 @@ public class PlayerData : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // Rolling the dice for random 1-6 int
+    public void RollDice()
+    {
+        die1 = Random.Range(0, 6);
+        die2 = Random.Range(0, 6);
+
+        Debug.Log("Die 1: " + die1 + ", Die 2: " +  die2);
     }
 }
