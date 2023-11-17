6041fc1920c7521d86a3e26d98801d139e7cb579
GavinTafoya <83233027+GavinTafoya@users.noreply.github.com>
2023-11-17 13:10:21 -0700

    Pieces should be moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,18 @@ public class PlayerMovement : MonoBehaviour
 {
     private int position, nextPosition;
     [SerializeField] Transform[] positions;
-    private GameObject currentPiece = new();
+    private GameObject currentPiece;
 
     private void Awake()
     {
+        currentPiece = new GameObject();
         position = nextPosition = 0;
     }
 
     private void Update()
     {
-        currentPiece.transform.position = Vector3.MoveTowards(currentPiece.transform.position, positions[nextPosition].position, .1f);
+        Vector3 movement = Vector3.MoveTowards(currentPiece.transform.position, positions[nextPosition].position, .1f);
+        currentPiece.transform.position = new Vector3(movement.x, currentPiece.transform.position.y, movement.z);
     }
 
     public void MovePiece(int numSpaces, GameObject piece)
